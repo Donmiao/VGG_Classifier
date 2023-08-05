a8a5751bdc8019543b623ae81169591f77afc47d
Aubrey <callmeaubrey@outlook.com>
2023-08-05 16:50:04 +0100

    Read me

--- README.md
+++ README.md
@@ -1,19 +1,22 @@
-# VGG_Classifier
+# VGG Classifier For Motion Images
  
 #### Train to classify the BVH textures with labels
 
 # How to use
 
 ### 1. Python environment
-The Project is based on python 3.6x
 Configure python environment according to requirements.txt
 
 ### 2. Datasets
-Divided the Train and test data with a ratio of 9:1 and store them to motion_data/test and motion_data/train.
+The datasets are generated from bvh files by VAT_Exporter.
+The local and world positions and rotations are extracted from the original bio-vision hierarchy files, and stored to tiff images.  The datasets are all standardised and sliced into uniform animation clips, as the clips are of different lengths and coordinates. Overlapping frames are used to enlarge the datasets. To extract the key information and remove the redundant data, the motion clips are down-sampled to 30 frames per second, and the sampling is interlaced.  Over 10,000 clips have been produced, with an overlap of 16 and a frame count (sliced clip length) of 80 empirically. Details could be found in VAT_exporter project.
+Then the train and test data are devided with a ratio of 9:1 and store them to motion_data/test and motion_data/train.
 
-### 3. Train 
-
-Train the datasets by running vgg_train.py(recommend) or running run.py for a user interface.
+### 3. Train
+Train the datasets by running vgg_train.py.
 
 ### 3.  Test the result
-modify the model_path in line 70 in vgg_classify.py and run vgg_classify.py.
+modify the model_path in line 70 in vgg_classify.py and run vgg_classify.py. The system will output the predicted label with the highest score.
+
+![img.png](img.png)
+
--- vgg_classify.py
+++ vgg_classify.py
@@ -9,7 +9,8 @@ from PIL import Image
 from torchvision.transforms import ToTensor
 
 # class_names = {"neutral": 0, "angry": 1, "depressed": 2, "drunk": 3, "happy": 4, "heavy": 5, "hurried": 6, "lazy" : 7, "old": 8, "proud": 9, "robot": 10, "sneaky": 11, "soldier": 12, "strutting": 13, "zombie": 14}
-class_names = ["neutral", "angry", "depressed", "happy", "heavy", "old", "proud","strutting"]
+class_names = ["neutral", "angry", "depressed", "happy", "heavy", "old", "proud", "strutting"]
+
 
 class Classify:
     def __init__(self, model_path, img_size, num_class, use_gpu=False):
@@ -56,7 +57,7 @@ class Classify:
             percentage_element = torch.nn.functional.softmax(output, dim=1)[0] * 100
             perc_element = percentage[i].item()
             class_name = class_names[i]
-            result_text+=(f"{class_name}: {perc_element}% \n")
+            result_text += (f"{class_name}: {perc_element}% \n")
             i += 1
 
         print(result_text)
@@ -67,68 +68,14 @@ class Classify:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-img_path', type=str, default='motion_data/angry-200.tiff', help='images path')
-    parser.add_argument('-model_path', type=str, default='checkpoint/epoch_42-best-acc_0.9312499761581421.pth', help='model path')
+    parser.add_argument('-model_path', type=str, default='checkpoint/epoch_42-best-acc_0.9312499761581421.pth',
+                        help='model path')
     parser.add_argument('-img_size', type=int, default=64, help='the size of image, mutiple of 32')
     parser.add_argument('-num_class', type=int, default=8, help='the number of class')
-    parser.add_argument('-gpu', default= False, help='use gpu or not')
+    parser.add_argument('-gpu', default=False, help='use gpu or not')
 
     opt = parser.parse_args()
 
-    classify = Classify(opt.model_path,opt.img_size,opt.num_class,opt.gpu)
+    classify = Classify(opt.model_path, opt.img_size, opt.num_class, opt.gpu)
     classify.init_model()
     classify.classify(opt.img_path)
-
-
-
-# if __name__ == '__main__':
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('-img_path', type=str, default='motion_data/angry-200.tiff', help='images path')
-#     parser.add_argument('-model_path', type=str, default='checkpoint/epoch_42-best-acc_0.9312499761581421.pth', help='model path')
-#     parser.add_argument('-img_size', type=int, default=64, help='the size of image, mutiple of 32')
-#     parser.add_argument('-num_class', type=int, default=8, help='the number of class')
-#     parser.add_argument('-gpu', default= False, help='use gpu or not')
-#
-#     opt = parser.parse_args()
-#
-#     # initialize vgg
-#     if opt.gpu:
-#         net = vgg_model.VGG(img_size=opt.img_size, input_channel=3, num_class=opt.num_class).cuda()
-#     else:
-#         net = vgg_model.VGG(img_size=opt.img_size, input_channel=3, num_class=opt.num_class)
-#
-#     # load model data
-#     net.load_state_dict(torch.load(opt.model_path))
-#     net.eval()
-#
-#     # img = Image.open(opt.img_path)
-#     # if len(img.split()) == 1:
-#     #     img = img.convert("RGB")
-#     # img = img.resize((opt.img_size, opt.img_size))
-#     # image_to_tensor = ToTensor()
-#     # img = image_to_tensor(img)
-#
-#     with rasterio.open(opt.img_path) as image:
-#         image_array = image.read()
-#     img = ToTensor()(image_array)
-#     img = np.array(img, np.float32).transpose(1, 2, 0)
-#     img = torch.tensor(img)
-#     img = img.unsqueeze(0)
-#
-#     if opt.gpu:
-#         img = img.cuda()
-#     output = net(img)
-#
-#     _, indices = torch.max(output, 1)
-#     percentage = torch.nn.functional.softmax(output, dim=1)[0] * 100
-#     perc = percentage[int(indices)].item()
-#     result = class_names[indices]
-#     print(f"Input File: {opt.img_path} \npredicted: {result}, percentage: {perc}% ")
-#
-#     print("====================")
-#     i = 0
-#     for output_element in output[0]:
-#         percentage_element = torch.nn.functional.softmax(output, dim=1)[0] * 100
-#         perc_element = percentage[i].item()
-#         result = class_names[i]
-#         print(f"{result}: {perc_element}% ")
-#         i+=1
